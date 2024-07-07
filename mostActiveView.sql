6bc0ddce1fc02e82eea0a4ace79e17d054fe2b85
MuhammadWaleed-Animations <69088842+MuhammadWaleed-Animations@users.noreply.github.com>
2024-07-07 13:10:40 +0500

    Delete mostActiveView.sql

--- mostActiveView.sql
+++
@@ -1,7 +0,0 @@
-create view mostActive
-as
-select top 10 u.[username],u.[name],u.[badges]
-from [dbo].[users] as u join [dbo].[discussion] as d on u.[username] = d.[uname]
-group by u.[username],u.[name],u.[badges]
-order by count(*) desc
-
