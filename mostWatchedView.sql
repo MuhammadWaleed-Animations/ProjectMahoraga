26db19379e09f12a6355375e333316d08372d412
MuhammadWaleed-Animations <69088842+MuhammadWaleed-Animations@users.noreply.github.com>
2024-07-07 13:12:06 +0500

    Delete mostWatchedView.sql

--- mostWatchedView.sql
+++
@@ -1,8 +0,0 @@
-create view mostWateched
-as
-select top 10 a.[name],a.[Descript],a.[releasedate],count(*) as viewss
-from [dbo].[Anime] as a join [dbo].[watch] as w on a.[AID] = w.[aniid]
-group by a.[AID],a.[name],a.[Descript],a.[releasedate]
-order by count(*) desc
-
---drop view mostWateched
